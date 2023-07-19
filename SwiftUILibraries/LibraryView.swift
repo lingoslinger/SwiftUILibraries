ca94f2b6348e1778a11c7763a0c8a307dcafccb2
Allan Evans <allan@agesoftwareconsultingllc.com>
2023-07-19 08:13:33 -0500

    Changed main view name to something more appropriate

--- SwiftUILibraries/LibraryView.swift
+++ SwiftUILibraries/LibraryView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LibraryView: View {
     @ObservedObject var dataStore = LibraryDataSource()
     
     var body: some View {
@@ -29,6 +29,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LibraryView()
     }
 }
